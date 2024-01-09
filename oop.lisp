09c99ddcf96db861cdeb3f348edb4b7b11c4f960
MatteScalda <matte.sca03@gmail.com>
2024-01-10 00:26:49 +0100

    Update oop.lisp
    prova delle funzioni
    sistemazione indentazione e parentesi
    
    Co-Authored-By: TeoMece <80129862+TeoMece@users.noreply.github.com>
    Co-Authored-By: Jacopogr <96066380+Jacopogr@users.noreply.github.com>

--- oop.lisp
+++ oop.lisp
@@ -2,53 +2,74 @@
 
 ;;; make-hash-table e gethash manipolano le hash tables in Common Lisp.
 ;;; La forma di class-spec è un dettaglio implementativo.
-(defparameter *defined-classes* (make-hash-table))
-
-(defun add-class (class-name parents-parts) 
-    (setf (gethash class-name *defined-classes*) parents-parts))
-
-(defun get-class-info (class-name) 
-    (gethash class-name *defined-classes*))
-
+(defparameter *classes-specs* (make-hash-table))
+(defun add-class-spec (name class-spec)
+    (setf (gethash name *classes-specs*) class-spec))
+(defun class-spec (name)
+    (gethash name *classes-specs*))
+    
 ;;; def-class: definisce la struttura di una classe e la
 ;;; memorizza in una locazione centralizzata (una variabile globale).
-(defun def-class (class-name parents (&rest parts)) (
-        (cond ((or (not (stringp class-name)) 
-                (equal class-name '()) 
+(defun def-class (class-name parents &rest parts) 
+        (cond ((or (stringp class-name)
                 (null class-name) 
                 (not (listp parents))) 
             (error (format nil "Error: Class-name or Parents invalid.")))
+            (t (add-class-spec class-name (list parents parts)) class-name)
         )
-        (add-class class-name (list parents parts))
-    )
 )
 
-(defun make (class-name &rest parts) (
+;;; make: crea un'istanza di una classe.
+(defun make (class-name &rest parts) 
     (cond 
-        ((not (is-class class-name)) (error (format nil "Error: Class-name invalid.")))
+        ((not (is-class class-name))) (error (format nil "Error: Class-name invalid."))
     )
-    (append (list 'instance)
+    (append (list 'oolinst)
             (list class-name)
-            (list parts))
+            (list parts)
     )
 )
 
-(defun is-class (class-name) (
+;;; is-class: verifica se un simbolo è il nome di una classe.
+(defun is-class (class-name) 
     (if (or 
-        ((null (get-class-info class-name))) 
-        (not (stringp class-name))) 
-            (nil) 
-            (t)
-    )
-))
-
-(defun is-instance (value &optional (class-name t)) (
-    (cond(
-        (
-            (and   ((string= (first value) 'instance)) (equal class-name 't)) 
-            (t)
+            (null (class-spec class-name)) 
+            (not (stringp class-name))
         )
-    ))
-    (if (null (get-class-info class-name)) (nil) (t))
-))
-          
+        nil
+        t
+    )
+)
+
+;;; is-instance: restituisce T se l'oggetto passatogli è l'istanza
+;;; di una classe.
+(defun is-instance (value &optional (class-name T)) 
+    (cond ((and (equal (car value) 'OOLINST) 
+                (equal class-name 'T)) T) 
+          ((equal (cadr value) class-name) T) 
+          ;; Ereditarietà 
+          ((member class-name (cadr (get-class-spec (cadr value)))) T))
+)
+;;; field: restituisce il valore di un campo di un'istanza.
+(defun field (instance field-name)
+    (cond ((or 
+        (not (is-instance instance))
+        (null field-name))
+        (error "Error: Instance-name or Field-name invalid."))
+        (t
+            (let ((fields (first(third instance))))
+                (extract-field fields field-name)
+            )
+        )
+    )
+)
+
+;;; extract-field: estrae il valore di un campo //TODO: non funziona 
+(defun extract-field (fields field-name)
+    (cond
+        ((null fields) (error "Error: Field-name not found."))
+        ((string= (first (first fields)) field-name) (second (first fields)))
+        (t (extract-field (rest fields) field-name))))
+
+;;//TODO: implementare field*
+
